019f483ac44211f1f751fd73f8a94f3e4b14aa16
Raphael Frey <rfreyp@gmail.com>
2021-07-12 16:40:23 -0700

    fixed user input

--- main.cpp
+++ main.cpp
@@ -4,21 +4,6 @@
 #include <utility>
 
 
-void printWelcome(){
-    for(int i = 0;i<80;i++){
-        std::cout<<'*';
-    }
-    std::cout<<std::endl;
-    std::cout<<"|    |   |  ______           ___    __             ______"<<std::endl;
-    std::cout<<"|    |   | |       |        /      /  \\    /\\ /\\  |      "<<std::endl;
-    std::cout<<"|    |   | |______ |       /      /    \\  / \\/  \\ |______"<<std::endl;
-    std::cout<<" \\   |   / |       |       \\      \\    /  |  |  | |      "<<std::endl;
-    std::cout<<"  \\_/\\_/   |______ |______  \\___   \\__/   |  |  | |______"<<std::endl;
-    for(int i = 0;i<80;i++){
-        std::cout<<'*';
-    }
-    std::cout<<std::endl;
-}
 std::vector<char> stringToVector(std::string s){
     std::vector<char> charVec(0);
     for(char c: s){
@@ -43,18 +28,18 @@ int main(){
         std::cout<<'*';
     }
     std::cout<<std::endl;
-    std::cout<<"welcome to the Enigma,"<<std::endl;
-    std::cout<<"enter the alphabet used, press enter to use default.  use the style ABCDEFG"<<std::endl;
+    std::cout<<"Welcome to the Enigma"<<std::endl;
+    std::cout<<"Enter a custom alphabet, or press enter to use default.  Use the style ABCDEFG to enter a new alphabet."<<std::endl;
     std::getline(std::cin,text);
     if(!text.empty()){
         e.setAlphabet(text);
     }
-    std::cout<<"enter the plugboard pairs by pairing letters to be connected after eachother eg. ACDB. To use default press enter"<<std::endl;
+    std::cout<<"Enter the plugboard pairs by pairing letters to be connected after eachother eg. ACDB. To use default (no connections) press enter."<<std::endl;
     std::getline(std::cin,text);
     if(!text.empty()){
         e.setPlugboard(text);
     }
-    std::cout<<"enter rotor seed or leave blank for random"<<std::endl;
+    std::cout<<"Enter rotor randomizer seed or leave blank to generate a random seed"<<std::endl;
     std::getline(std::cin,text);
     if(text.empty()){
         seed = 0;
@@ -64,7 +49,7 @@ int main(){
             getline(std::cin, text);
         }
     }
-    std::cout<<"enter rotor amount or leave blank for 3"<<std::endl;
+    std::cout<<"Enter rotor amount or leave blank for 3"<<std::endl;
     std::getline(std::cin,text);
     if(text.empty()){
         rotorCount = 3;
@@ -74,7 +59,7 @@ int main(){
             getline(std::cin, text);
         }
     }
-    std::cout<<"enter Text"<<std::endl<<std::endl;
+    std::cout<<"Enter text which should be encrypted"<<std::endl<<std::endl;
     std::getline(std::cin,text);
     e.encrypt(text,rotorCount,seed);
 }
